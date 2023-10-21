36c23fe9e8533681d4ac9b49646ce9e1c38d61d8
Karol Miklasz <karol3wpl@gmail.com>
2023-10-21 15:27:28 +0200

    Add moul4zad2.py

---
+++ modul4zad2.py
@@ -0,0 +1,9 @@
+import logging
+
+operation = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ")
+
+a = float(input("Podaj składnik 1: "))
+b = float(input("Podaj składnik 2: "))
+
+result = 0
+
